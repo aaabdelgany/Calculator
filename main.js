7b95a8f348639b88a7d88bfbca9851f5bfdb2a99
aaabdelgany <42511430+aaabdelgany@users.noreply.github.com>
2021-03-04 19:59:35 -0500

    size

--- main.js
+++ main.js
@@ -2,13 +2,16 @@ document.addEventListener('DOMContentLoaded', function() {
     const nums=document.querySelectorAll('.num');
     const input=document.querySelector('input');
     const ops=document.querySelectorAll('.op');
+    const float=document.querySelector('#float')
     let curr='';
     let stepOne='';
     let stepTwo='';
     let operator='';
     for(let num of nums){
         num.addEventListener('click',()=>{
+
             curr+=num.innerHTML;
+
             input.value=curr;
             if(stepOne!==''){
                 stepTwo=curr;
@@ -17,6 +20,7 @@ document.addEventListener('DOMContentLoaded', function() {
     }
     for(let op of ops){
         op.addEventListener('click',()=>{
+            float.disabled=false;
             if(stepOne===''){
                 stepOne=input.value;
                 curr='';
@@ -25,10 +29,18 @@ document.addEventListener('DOMContentLoaded', function() {
                 curr='';   
             }else{
                 let val=calc(stepOne,stepTwo,operator);
-                curr='';
-                stepOne=val;
-                stepTwo='';
-                input.value=val;
+                if(val!=='Zero!'){
+                    curr='';
+                    stepOne=val;
+                    stepTwo='';
+                    input.value=val;
+                }else{
+                    curr=''
+                    stepOne='';
+                    stepTwo=''
+                    input.value=`You can't divide by zero, Bud.`
+                }
+
             }
             operator=op.innerHTML;
 
@@ -40,6 +52,7 @@ document.addEventListener('DOMContentLoaded', function() {
         stepTwo='';
         operator='';
         input.value='';
+        float.disabled=false;
     })
     document.querySelector('#posneg').addEventListener('click',()=>{
         if(curr[0]==='-'){
@@ -50,23 +63,30 @@ document.addEventListener('DOMContentLoaded', function() {
             input.value=curr;
         }
     })
-    document.querySelector('#float').addEventListener('click',()=>{
+
+    float.addEventListener('click',()=>{
         if(!curr.includes('.')){
             curr+='.'
             input.value=curr;
-
+            float.disabled=true;
         }
     })
     
     document.querySelector('#evaluate').addEventListener('click',()=>{
-
         if(stepTwo!==''){
             let val=calc(stepOne,stepTwo,operator);
-            curr=val;
-            stepOne='';
-            stepTwo='';
-            operator='';
-            input.value=curr;
+            if(val!=='Zero!'){
+                curr=val;
+                stepOne='';
+                stepTwo='';
+                operator='';
+                input.value=curr;
+            }else{
+                curr=''
+                stepOne='';
+                stepTwo=''
+                input.value=`You can't divide by zero, Bud.`
+            }
         }
     })
 
@@ -79,13 +99,16 @@ function calc(n1,n2,op){
     let num1=parseFloat(n1);
     let num2=parseFloat(n2);
     if(op==='/'){
-        return num1/num2;
+        if(num2===0){
+            return 'Zero!'
+        }
+        return (num1/num2).toFixed(4);
     }else if(op==='x'){
-        return num1*num2;
+        return (num1*num2).toFixed(4);
     }else if(op==='-'){
-        return num1-num2;
+        return (num1-num2).toFixed(4);
     }else if(op==='+'){
-        return num1+num2;
+        return (num1+num2).toFixed(4);
     }
 }
 
